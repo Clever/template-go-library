4ddc18699e6f9d9c7e3c37c86acbfbfd5e4376f0
Chris Martin <101210272+ChrisScotMartin@users.noreply.github.com>
2025-03-10 11:16:01 -0400

    clearer comment

--- example.go
+++ example.go
@@ -4,7 +4,8 @@ import (
 	"github.com/Clever/{{.AppName}}/internal/internalexample"
 )
 
-// Hello is a function that prints hello world in a convoluted way
+// Hello is a function that prints hello world in a convoluted way by calling a function from an
+// internal package.
 func Hello() {
 	internalexample.InternalHello()
 }
